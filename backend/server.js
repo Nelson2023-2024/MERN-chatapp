494f9c071af138526bff452a14eb4b32fd304cc1
Nelson Ouya <132818949+Nelson2023-2024@users.noreply.github.com>
2024-10-12 21:11:54 +0300

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
+//@ts-nocheck
 import { configDotenv } from "dotenv";
+import path from "path";
 import express from "express";
 import { authRoutes } from "./routes/authRoutes.js";
 import { connectToMongoDB } from "./db/connectToMongoDB.js";
@@ -11,6 +13,8 @@ configDotenv();
 
 const PORT = process.env.PORT;
 
+const __dirname = path.resolve();
+
 //extract file from json body
 app.use(express.json());
 app.use(cookieParser());
@@ -18,6 +22,13 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 app.use("/api/users", userRoutes);
+
+//gives us the absolute path to the root folder
+app.use(express.static(path.join(__dirname, "/frontend/dist"))); //used to serve static files
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, async () => {
   await connectToMongoDB();
